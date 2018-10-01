ce21c6f977c36a8be80bf40e2505554673c3e6c1
PilarArroyave <mdelpilar.arroyave@udea.edu.co>
2018-09-30 21:06:54 -0500

    Selector de pais con string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,87 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Banco Interamericano';
+
+  countries = [
+              {
+                "name": "Argentina",
+                "code": "ar"
+              },
+              {
+                "name": "Bolivia",
+                "code": "bo"
+              },
+              {
+                "name": "Brasil",
+                "code": "br"
+              },
+              {
+                "name": "Chile",
+                "code": "cl"
+              },
+              {
+                "name": "Colombia",
+                "code": "co"
+              },
+              {
+                "name": "Costa Rica",
+                "code": "cr"
+              },
+              {
+                "name": "Cuba",
+                "code": "cu"
+              },
+              {
+                "name": "Ecuador",
+                "code": "ec"
+              },
+              {
+                "name": "El Salvador",
+                "code": "sv"
+              },
+              {
+                "name": "Guatemala",
+                "code": "gt"
+              },
+              {
+                "name": "Haití",
+                "code": "ht"
+              },
+              {
+                "name": "Honduras",
+                "code": "hn"
+              },
+              {
+                "name": "México",
+                "code": "mx"
+              },
+              {
+                "name": "Nicaragua",
+                "code": "ni"
+              },
+              {
+                "name": "Panamá",
+                "code": "pa"
+              },
+              {
+                "name": "Paraguay",
+                "code":  "py"
+              },
+              {
+                "name": "Perú",
+                "code": "pe"
+              },
+              {
+                "name": "República Dominicana",
+                "code": "do"
+              },
+              {
+                "name": "Uruguay",
+                "code": "uy"
+              },
+              {
+                "name": "Venezuela",
+                "code":  "ve"
+              }
+  ];
 }
